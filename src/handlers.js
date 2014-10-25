30c184c682b51a3bf6954c258d713010860a1f24
James Wilson <james@jwilson.us>
2014-10-25 09:26:44 -0500

    Tabs to spaces

--- src/handlers.js
+++ src/handlers.js
@@ -4,15 +4,15 @@ var _ = require('lodash');
 var handlers = {};
 
 handlers.getUsers = function (req, res, next) {
-	res.send(db.users);
-	next();
+  res.send(db.users);
+  next();
 };
 
 handlers.getUser = function (req, res, next) {
-	var user = _.find(db.users, { id: parseInt(req.params.id, 10) });
+  var user = _.find(db.users, { id: parseInt(req.params.id, 10) });
 
-	res.send(user);
-	next();
+  res.send(user);
+  next();
 };
 
 module.exports = handlers;
--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ var handlers = require('./handlers');
 var server = {};
 
 server.start = function () {
-	var instance = restify.createServer();
+  var instance = restify.createServer();
 
-	instance.get('/user', handlers.getUsers);
-	instance.get('/user/:id', handlers.getUser);
+  instance.get('/user', handlers.getUsers);
+  instance.get('/user/:id', handlers.getUser);
 
-	instance.listen(9000, function () {
-		console.log('%s listening at %s', instance.name, instance.url);
-	});
+  instance.listen(9000, function () {
+    console.log('%s listening at %s', instance.name, instance.url);
+  });
 };
 
 module.exports = server;
